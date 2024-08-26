c1f67f6f92fb13aa652492c6c52d9a88037380de
Pedro Garcia <pedroantoniodefariagarcia@gmail.com>
2024-08-26 13:12:55 -0300

    finalizando edicoes

---
+++ js/funcoesPagInicial.js
@@ -0,0 +1,5 @@
+//Função da modal de atendimento
+
+document.getElementById('horarioCirculo').addEventListener('click', function() {
+    document.getElementById('horarioTexto').classList.toggle('show');
+});
